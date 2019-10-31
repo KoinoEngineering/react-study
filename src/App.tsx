7441ac6c37a414622947bbae75c398772d2fb57b
KoinoEngineering <email@koino.engineering>
2019-10-31 23:52:05 +0900

    #1 material-ui適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import logo from "./logo.svg";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    App: {
+        textAlign: "center"
+    },
+    AppHeader: {
+        alignItems: "center",
+        backgroundColor: "#282c34",
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        fontSize: "calc(10px + 2vmin)",
+        justifyContent: "center",
+        minHeight: "100vh",
+    },
+    AppLink: {
+        color: "#09d3ac",
+    },
+    Applogo: {
+        height: "40vmin",
+    },
+}
+);
 
 const App: React.FC = () => {
+
+    const classes = useStyles();
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className={classes.App}>
+            <header className={classes.AppHeader}>
+                <img src={logo} className={classes.Applogo} alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
         </p>
                 <a
-                    className="App-link"
+                    className={classes.AppLink}
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
