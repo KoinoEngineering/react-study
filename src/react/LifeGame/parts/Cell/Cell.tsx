953c09454bbd342703c23df4724c30a729697849
KoinoEngineering <email@koino.engineering>
2019-11-03 19:33:22 +0900

    #1 Cellを整理

--- src/react/LifeGame/parts/Cell/Cell.tsx
+++ src/react/LifeGame/parts/Cell/Cell.tsx
@@ -1,6 +1,7 @@
 import { Button, colors, WithStyles, createStyles, withStyles } from "@material-ui/core";
 import classNames from "classnames";
 import React from "react";
+import { IDispatchable, DISPATCHABLE } from "../../../../core/Interfaces/Dispatchable";
 
 
 interface IStyleProps {
@@ -26,88 +27,31 @@ const styles = createStyles({
 });
 
 export interface ICellProps extends WithStyles<typeof styles> {
-    y: number;
-    x: number;
-    ref?: React.Ref<HTMLButtonElement>;
+    alive: IDispatchable<boolean>;
 }
 
-export interface ICellState {
-    alive: boolean;
-}
-
-class Cell extends React.PureComponent<ICellProps, ICellState> {
+class Cell extends React.PureComponent<ICellProps> {
     constructor(props: ICellProps) {
         super(props);
         //初期化
         this.state = { alive: false };
     }
 
-    private Alive() {
-        this.setState({ ...this.state, alive: true });
-    }
-
-    private Dead() {
-        this.setState({ ...this.state, alive: false });
-    }
-
-    private HandleClickButton(alive: boolean) {
+    private HandleClickButton(alive: IDispatchable<boolean>) {
         return () => {
-            this.setState({ ...this.state, alive });
+            alive[DISPATCHABLE.DISPATCHER](!alive[DISPATCHABLE.VALUE]);
         };
     }
 
-    public UpdateGeneration(field: boolean[][]) {
-        const {
-            y,
-        } = this.props;
-        const {
-            alive
-        } = this.state;
-        // modで-1するのが面倒なので補正用の配列を作る
-        const heightDiffArray = [field.length - 1, 0, 1];
-        const widthDiffArray = [field[y].length - 1, 0, 1];
-        let neighborAliveCount = 0;
-        for (let h = 0; h < 3; h++) {
-            for (let w = 0; w < 3; w++) {
-                const diffHeight = (this.props.y + heightDiffArray[h]) % field.length;
-                const diffWidth = (this.props.x + widthDiffArray[w]) % field[y].length;
-                if (field[diffHeight][diffWidth]) {
-                    neighborAliveCount++;
-                }
-            }
-        }
-
-        // 状態更新
-        if (alive) {
-            if (neighborAliveCount <= 1) {
-                // 過疎
-                this.Dead();
-            } else if ((neighborAliveCount === 2 || neighborAliveCount === 3)) {
-                // 生存（定義に存在するので分岐を作るが維持なので何もしない）
-            } else if (neighborAliveCount >= 4) {
-                // 過密
-                this.Dead();
-            }
-        } else {
-            // 近傍の生が3の時だけ誕生
-            if (neighborAliveCount === 3) {
-                // 誕生
-                this.Alive();
-            }
-        }
-    }
 
     render() {
         const {
             classes,
-        } = this.props;
-        const {
             alive
-        } = this.state;
+        } = this.props;
         return <Button
-            ref={this.props.ref}
-            className={classNames(classes.root, { alive })}
-            onClick={this.HandleClickButton(!alive)}
+            className={classNames(classes.root, { alive: alive[DISPATCHABLE.VALUE] })}
+            onClick={this.HandleClickButton(alive)}
         />;
     }
 }
