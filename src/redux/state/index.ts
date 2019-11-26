effdc438bfc38ee6ea615638927c2934d24a5563
KoinoEngineering <email@koino.engineering>
2019-11-26 09:23:35 +0900

    #9 action/state

--- src/redux/state/index.ts
+++ src/redux/state/index.ts
@@ -1,10 +1,14 @@
 import { RecordOf, Record } from "immutable";
 import { IBubblingInRedux, BubblingInRedux } from "./BubblingInRedux";
+import { SimpleCombineState } from "../pages/RecursiveCombinedReducer/SimpleCombine/SimpleCombine";
+import { initialSimpleCombineState } from "./RecursiveCombinedReducer/SimpleCombine/SimpleCombineState";
 
 export interface IState {
     bubblingInRedux: RecordOf<IBubblingInRedux>
+    recursiveCombinedReducer: SimpleCombineState
 }
 
 export const State = Record<IState>({
-    bubblingInRedux: BubblingInRedux()
+    bubblingInRedux: BubblingInRedux(),
+    recursiveCombinedReducer: initialSimpleCombineState()
 });
