381f94429e61d607a48bf61c20bf61b8930945ee
KoinoEngineering <email@koino.engineering>
2019-11-26 23:48:51 +0900

    #9 reducer

--- src/redux/reducer/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChildReducer.ts
+++ src/redux/reducer/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChildReducer.ts
@@ -1,21 +1,24 @@
 import { Reducer } from "redux";
-import { INumberBoxState } from "../../../../../../react/common/NumberBox/NumberBox";
 import { IAction } from "../../../../../action";
 import { SimpleCombineGrandChildActions } from "../../../../../action/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild";
-import { SimpleCombineGrandChildState } from "../../../../../pages/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChild";
+import { SimpleCombineGrandChildState, ReduxNumberBoxState } from "../../../../../pages/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChild";
 
 export interface SimpleCombineGrandChildPayload {
     target: keyof SimpleCombineGrandChildState,
-    value: INumberBoxState;
+    value: ReduxNumberBoxState;
 }
 
 export const SimpleCombineGrandChildReducer: Reducer<SimpleCombineGrandChildState, IAction> = (state: SimpleCombineGrandChildState = {
-    numberA: "10",
-    numberB: "10",
-}, action: IAction) => {
+    numberA: {
+        state: "10",
+    },
+    numberB: {
+        state: "10",
+    },
+}, action: IAction<SimpleCombineGrandChildPayload>): SimpleCombineGrandChildState => {
     switch (action.type) {
         case SimpleCombineGrandChildActions.SIMPLE_COMBINE_GRANDCHILD_UPDATE:
-            return { ...state, [action.payload.target]: action.payload.value };
+            return { ...state, [action.payload.target]: { ...action.payload.value } };
         default:
             return state;
     }
