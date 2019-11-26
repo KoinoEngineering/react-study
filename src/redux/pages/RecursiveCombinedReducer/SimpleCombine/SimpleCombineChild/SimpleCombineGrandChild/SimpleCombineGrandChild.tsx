6d973b7d54c08f61cab9ff2c336caa4c2a76686d
KoinoEngineering <email@koino.engineering>
2019-11-26 23:47:19 +0900

    #9 GrandChildを修正dispatchしてconnect

--- src/redux/pages/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChild.tsx
+++ src/redux/pages/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChild.tsx
@@ -1,29 +1,60 @@
 import React from "react";
-import NumberBox, { INumberBoxProps, INumberBoxState } from "../../../../../../react/common/NumberBox/NumberBox";
+import { connect, MapDispatchToProps } from "react-redux";
+import { Dispatch } from "redux";
+import NumberBox, { INumberBoxProps } from "../../../../../../react/common/NumberBox/NumberBox";
+import { IAction } from "../../../../../action";
+import { SimpleCombineGrandChildActions } from "../../../../../action/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild";
+import { SimpleCombineGrandChildPayload } from "../../../../../reducer/RecursiveCombinedReducer/SimpleCombine/SimpleCombineChild/SimpleCombineGrandChild/SimpleCombineGrandChildReducer";
 
 export interface SimpleCombineGrandChildProps {
     numberA: INumberBoxProps;
     numberB: INumberBoxProps;
+    dispatch: Dispatch<IAction<SimpleCombineGrandChildPayload>>;
 }
 
+export type ReduxNumberBoxState = Pick<INumberBoxProps, "state">;
 export interface SimpleCombineGrandChildState {
-    numberA: INumberBoxState;
-    numberB: INumberBoxState;
+    numberA: ReduxNumberBoxState;
+    numberB: ReduxNumberBoxState;
 }
 
-export class SimpleCombineGrandChild extends React.PureComponent<SimpleCombineGrandChildProps>{
+class SimpleCombineGrandChild extends React.PureComponent<SimpleCombineGrandChildProps>{
+
+    private updateValueCreator = (dispatch: Dispatch<IAction>, prevState: string): React.Dispatch<React.SetStateAction<string>> => {
+        return (value: string | ((prevState: string) => string)) => {
+            if (typeof value === "function" && prevState === undefined) {
+                throw new Error("reset function needs prevState");
+            }
+            dispatch({
+                payload: {
+                    value: typeof value === "function" ? value(prevState) : value
+                },
+                type: SimpleCombineGrandChildActions.SIMPLE_COMBINE_GRANDCHILD_UPDATE,
+            });
+        };
+    }
+
     public render() {
         const {
             numberA,
-            numberB
+            numberB,
+            dispatch,
         } = this.props;
         return <div>
             <div>
-                <NumberBox {...numberA} />
+                <NumberBox {...numberA} dispatch={this.updateValueCreator(dispatch, numberA.state)} />
             </div>
             <div>
-                <NumberBox {...numberB} />
+                <NumberBox {...numberB} dispatch={this.updateValueCreator(dispatch, numberB.state)} />
             </div>
         </div>;
     }
 }
+
+const mapDispatchToProps: MapDispatchToProps<Pick<SimpleCombineGrandChildProps, "dispatch">, SimpleCombineGrandChildProps> = (dispatch: SimpleCombineGrandChildProps["dispatch"]) => {
+    return {
+        dispatch
+    };
+};
+
+export default connect(null, mapDispatchToProps)(SimpleCombineGrandChild);
