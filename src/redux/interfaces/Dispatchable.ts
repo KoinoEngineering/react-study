1862ec9d043151e25b412afd86593f358bb7fe20
KoinoEngineering <email@koino.engineering>
2019-11-26 23:04:05 +0900

    #9 dispatchableにpayloadを追加

--- src/redux/interfaces/Dispatchable.ts
+++ src/redux/interfaces/Dispatchable.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from "redux";
 import { IAction } from "../action";
 
-export interface Dispatchable {
-    dispatch: Dispatch<IAction>;
+export interface Dispatchable<P = any> {
+    dispatch: Dispatch<IAction<P>>;
 }
