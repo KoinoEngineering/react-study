33e22ddaa944b072b97d87c64001a40022f7f902
Koino Engineering <email@koino.engineering>
2020-01-12 23:46:15 +0900

    #25 implements HanoiSettings

--- src/Pages/Hanoi/Hanoi.tsx
+++ src/Pages/Hanoi/Hanoi.tsx
@@ -1,13 +1,22 @@
+import { List } from "immutable";
 import React from "react";
 import { childDispatcherResetFactory } from "../../common/Dispatch";
 import { IPropsBaseReset } from "../../core/Interfaces/Props";
 import HanoiSettings, { IHanoiSettingsState } from "./HanoiSettings/HanoiSettings";
-import Towers, { ITowersState, TowersState } from "./Towers/Towers";
+import Towers, { ITowersState, TowersState, ITowers } from "./Towers/Towers";
 
 export interface IHanoiState {
     settings: IHanoiSettingsState;
     towers: ITowersState;
 }
+
+export const defaultTowers = (_calss: number): ITowers => {
+    return {
+        a: List(new Array(_calss).fill(0)),
+        b: List(new Array(_calss).fill(0)),
+        c: List(new Array(_calss).fill(0)),
+    };
+};
 
 export const initHanoiState = (): IHanoiState => {
     return {
@@ -16,19 +25,27 @@ export const initHanoiState = (): IHanoiState => {
             goto: "c",
             now: "a",
         },
-        towers: new TowersState()
+        towers: new TowersState({
+            ...defaultTowers(3),
+            a: List([1, 2, 3]),
+        })
     };
 };
 
 type IHanoiProps = IPropsBaseReset<IHanoiState>;
 const Hanoi: React.FC<IHanoiProps> = (props: IHanoiProps) => {
 
+    const towersDispatch = childDispatcherResetFactory(props.dispatch, props.state, "towers");
     return <div>
         <div>
-            <HanoiSettings state={props.state.settings} dispatch={childDispatcherResetFactory(props.dispatch, props.state, "settings")} />
+            <HanoiSettings
+                state={props.state.settings}
+                dispatch={childDispatcherResetFactory(props.dispatch, props.state, "settings")}
+                towersDispatch={towersDispatch}
+            />
         </div>
         <div>
-            <Towers state={props.state.towers} dispatch={childDispatcherResetFactory(props.dispatch, props.state, "towers")} />
+            <Towers state={props.state.towers} dispatch={towersDispatch} />
         </div>
     </div>;
 };
--- src/Pages/Hanoi/HanoiSettings/HanoiSettings.tsx
+++ src/Pages/Hanoi/HanoiSettings/HanoiSettings.tsx
@@ -1,19 +1,98 @@
+import ClassNames from "classnames";
+import { makeStyles, MenuItem, Select } from "@material-ui/core";
+import { List } from "immutable";
 import React from "react";
 import MyDispatch from "../../../core/Interfaces/MyDispatch";
-import { ITowers } from "../Towers/Towers";
+import { IPropsBaseReset } from "../../../core/Interfaces/Props";
+import { defaultTowers } from "../Hanoi";
+import { ITowersState, TowersState } from "../Towers/Towers";
 
 export interface IHanoiSettingsState {
     class: number;
-    now: keyof ITowers;
-    goto: keyof ITowers;
+    now: keyof ITowersState;
+    goto: keyof ITowersState;
 }
-interface HanoiSettingsProps {
-    state: IHanoiSettingsState;
-    dispatch: MyDispatch<IHanoiSettingsState>["Reset"]
+interface HanoiSettingsProps extends IPropsBaseReset<IHanoiSettingsState> {
+    divProps?: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+    towersDispatch: MyDispatch<ITowersState>["Reset"];
 }
 
+const HanoiKeys: (keyof ITowersState)[] = ["a", "b", "c"];
+
 const HanoiSettings: React.FC<HanoiSettingsProps> = (props: HanoiSettingsProps) => {
-    return <div>{JSON.stringify(props)}</div>;
+
+    const paddingStyles = makeStyles({
+        bottom5: {
+            paddingBottom: 5
+        }
+    })();
+    const selectStyles = makeStyles({
+        root: {
+            paddingBottom: 5,
+            paddingTop: 5,
+        }
+    })();
+
+    const borderStyles = makeStyles({
+        solidBlack1: {
+            border: "solid black 1px"
+        }
+    })();
+
+
+    const handleChildSelectChangeFactory = <K extends keyof IHanoiSettingsState>(key: K) => {
+        return (
+            event: React.ChangeEvent<{ name?: string; value: unknown }>,
+        ) => {
+            props.dispatch((prevState) => {
+                return {
+                    ...prevState,
+                    [key]: event.target.value as IHanoiSettingsState[K]
+                };
+            });
+        };
+    };
+
+
+    const handleClassChange = (
+        event: React.ChangeEvent<{ name?: string; value: unknown }>,
+    ) => {
+        const _class = event.target.value as number;
+        handleChildSelectChangeFactory("class")(event);
+        props.towersDispatch(() => {
+            return new TowersState({
+                ...defaultTowers(_class),
+                [props.state.now]: List(new Array(_class)
+                    .fill(0)
+                    .map((_, idx) => {
+                        return idx + 1;
+                    }))
+            });
+        });
+    };
+    return <div  {...props.divProps} className={ClassNames(props.divProps?.className, borderStyles.solidBlack1)}>
+        <div><h3>Settings</h3></div>
+        <div className={paddingStyles.bottom5}>
+            段数 :  <Select classes={selectStyles} value={props.state.class} onChange={handleClassChange} variant={"outlined"}>
+                {new Array<number>(100).fill(0).map((_, idx) => {
+                    const tmp = idx + 1;
+                    return <MenuItem key={"class_" + tmp} value={tmp}>{tmp}</MenuItem>;
+                })}
+            </Select>
+        </div>
+        <div className={paddingStyles.bottom5}>
+            {"現在地: " + props.state.now}
+        </div>
+        <div className={paddingStyles.bottom5}>
+            移動先 : <Select classes={selectStyles} value={props.state.goto} onChange={handleChildSelectChangeFactory("goto")} variant={"outlined"}>
+                {HanoiKeys.filter((key: keyof ITowersState) => {
+                    return key !== props.state.now;
+                }).map((key: keyof ITowersState) => {
+                    return <MenuItem key={key} value={key} >{key}</MenuItem>;
+                })}
+            </Select>
+        </div>
+    </div>;
 };
 
 export default HanoiSettings;
