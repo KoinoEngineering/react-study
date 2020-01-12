3b36ff537dc3b1ce73ccf75db10eb8d9261902d3
Koino Engineering <email@koino.engineering>
2020-01-12 21:53:11 +0900

    #25 add PropsBase

--- src/core/Interfaces/Props.ts
+++ src/core/Interfaces/Props.ts
@@ -1,1 +1,13 @@
+import MyDispatch from "./MyDispatch";
+
 export type IDRequired<P = {}> = P & { id: string };
+
+export type IPropsBase<S> = {
+    state: S;
+    dispatch: MyDispatch<S>["Set"];
+}
+
+export type IPropsBaseReset<S> = {
+    state: S;
+    dispatch: MyDispatch<S>["Reset"];
+} 
