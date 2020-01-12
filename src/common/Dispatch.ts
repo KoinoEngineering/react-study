b7275f13983619b539e66f05a4ebc86f16e214b1
Koino Engineering <email@koino.engineering>
2020-01-12 22:12:25 +0900

    #25 add childDispatcherResetFactory

--- src/common/Dispatch.ts
+++ src/common/Dispatch.ts
@@ -9,3 +9,15 @@ export const childDispatcherFactory = <S, K extends keyof S>(dispatch: MyDispatc
     };
 };
 
+type Resetter<T> = (prevState: T) => T
+
+export const childDispatcherResetFactory = <S, K extends keyof S>(dispatch: MyDispatch<S>["Reset"], state: S, key: K) => {
+    return (resetter: Resetter<S[K]>) => {
+        dispatch((prevState) => {
+            return {
+                ...prevState,
+                [key]: resetter(state[key])
+            };
+        });
+    };
+};
